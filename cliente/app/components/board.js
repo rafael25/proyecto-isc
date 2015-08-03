eb56418691e61edbd5a949ea67782b3afdeeb56f
Rafael Viveros <rafaelvb25@gmail.com>
2015-08-03 03:54:12 -0500

    Cliente: Agrega posibilidad de cambiar la dirección de los barcos

--- cliente/app/components/board.js
+++ cliente/app/components/board.js
@@ -6,9 +6,9 @@ module.exports = {
     props: ['conn', 'user', 'changeComponent'],
 
     created: function () {
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < this.$data.boardSize; i++) {
             var boxList = [];
-            for ( var j = 0; j < 10; j++) {
+            for ( var j = 0; j < this.$data.boardSize; j++) {
                 var box = {
                     x: j,
                     y: i,
@@ -22,35 +22,28 @@ module.exports = {
 
     data: function () {
         return {
+            boardSize: 10,
+
             boxes: [],
 
             ships: [
-                {size: 4, x: 0, y: 0, dir: 'v', positioned: false},
-                {size: 3, x: 0, y: 0, dir: 'v', positioned: false},
-                {size: 2, x: 0, y: 0, dir: 'v', positioned: false},
-                {size: 1, x: 0, y: 0, dir: 'v', positioned: false}
+                {size: 4, x: 0, y: 0, dir: 'v', positioned: false, name: 'grande'},
+                {size: 3, x: 0, y: 0, dir: 'h', positioned: false, name: 'mediano'},
+                {size: 2, x: 0, y: 0, dir: 'v', positioned: false, name: 'pequeño'},
+                {size: 1, x: 0, y: 0, dir: 'v', positioned: false, name: 'minusculo'}
             ],
 
-            ship: ''
+            shipSize: '',
         };
     },
 
     methods: {
         boxSelected: function(box) {
-            if (this.ship <= 0 || this.ship > 4) {
+            if (this.shipSize <= 0 || this.shipSize > 4) {
                 console.log('Barco fuera de proporciones');
                 return;
             }
-            var inicio = box.y;
-            var fin = box.y + parseInt(this.ship, 10) - 1;
-            if (fin > 9) {
-                console.log('Barco fuera de tablero inicio:' + inicio + ' fin:' + fin);
-                return;
-            }
-            this.locateShip(this.ship, box, 'v');
-        },
-
-        locateShip: function (shipSize, box, dir) {
+            var shipSize = parseInt(this.shipSize, 10);
             var ship = (function (arr, size) {
                 for (var i = 0; i < arr.length; i++) {
                     if (arr[i].size == size) {
@@ -58,37 +51,66 @@ module.exports = {
                     }
                 }
             })(this.ships, shipSize);
-
-            // TODO: Mover esto a un metodo removeShip
-            // TODO: Quitar el barco de su antigua ubicacion solo si
-            // la nueva es valida
-            if (ship.positioned) {
-                var inicio = ship.y;
-                var fin = ship.y + ship.size - 1;
-                for (var i = inicio; i <= fin; i++) {
-                    if (ship.dir === 'v') {
-                        this.boxes[i][ship.x].used = false;
-                    } else if (ship.dir === 'h') {
-                        this.boxes[ship.y][i].used = false;
-                    }
-                }
+            var sPoint = (ship.dir === 'v')? box.y : box.x;
+            var inicio = sPoint;
+            var fin = sPoint + shipSize - 1;
+            if (fin > this.boardSize - 1) {
+                console.log('Barco fuera de tablero inicio:' + inicio + ' fin:' + fin);
+                return;
             }
+            if (ship.positioned) {
+                this.removeShip(ship);
+            }
+            this.locateShip(ship, box);
+        },
 
-            var inicio = box.y;
-            var fin = box.y + parseInt(shipSize, 10) - 1;
+        locateShip: function (ship, box) {
+            var sPoint = (ship.dir === 'v')? box.y : box.x;
+            var inicio = sPoint;
+            var fin = sPoint + ship.size - 1;
 
             // TODO: Impedir que el barco a ubicar ocupe casillas de otro barco
             for (var i = inicio; i <= fin; i++) {
-                if (dir === 'v') {
+                if (ship.dir === 'v') {
                     this.boxes[i][box.x].used = true;
-                } else if (dir === 'h') {
+                } else if (ship.dir === 'h') {
                     this.boxes[box.y][i].used = true;
                 }
             }
             ship.x = box.x;
             ship.y = box.y;
-            ship.dir = dir;
             ship.positioned = true;
+        },
+
+        removeShip: function (ship) {
+            var sPoint = (ship.dir === 'v')? ship.y : ship.x;
+            var inicio = sPoint;
+            var fin = sPoint + ship.size - 1;
+            for (var i = inicio; i <= fin; i++) {
+                if (ship.dir === 'v') {
+                    this.boxes[i][ship.x].used = false;
+                } else if (ship.dir === 'h') {
+                    this.boxes[ship.y][i].used = false;
+                }
+            }
+        },
+
+        rotateShip: function (ship, dir) {
+            if (! ship.positioned) {
+                return;
+            }
+            var sPoint = (dir === 'v')? ship.y : ship.x;
+            var fin = sPoint + ship.size - 1;
+            if (fin > this.boardSize - 1) {
+                console.log('Barco fuera de tablero inicio:' + sPoint + ' fin:' + fin);
+                return;
+            }
+            var box = {x: ship.x, y: ship.y};
+            if (ship.positioned) {
+                this.removeShip(ship);
+            }
+            ship.dir = dir;
+            this.locateShip(ship, box);
         }
     }
 }
